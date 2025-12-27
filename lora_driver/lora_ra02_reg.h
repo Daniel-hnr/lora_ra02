8432132c1d24c4d41083bb669ad20b1cfe5f97b4
Danial <daniel.hnr84@gmail.com>
2025-12-27 23:46:24 +0330

    init

---
+++ lora_driver/lora_ra02_reg.h
@@ -0,0 +1,24 @@
+#ifndef RA02_REG
+#define RA02_REG
+
+
+// Register table
+#define RA02_RegFifo 0x00
+#define RA02_RegOpMode 0x01
+#define RA02_RegFrMsb 0x06
+#define RA02_RegFrMid 0x07
+#define RA02_RegFrLsb 0x08
+#define RA02_RegPaConfig 0x09
+#define RA02_RegPaRamp 0x0A
+#define RA02_RegOcp 0x0B
+
+
+
+
+
+
+
+
+
+
+#endif
