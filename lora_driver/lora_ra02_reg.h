00af8a1e8aa9d7c2e2cee4760d3578509bfac947
Danial <daniel.hnr84@gmail.com>
2025-12-28 16:34:22 +0330

    add all registers addr

--- lora_driver/lora_ra02_reg.h
+++ lora_driver/lora_ra02_reg.h
@@ -11,9 +11,42 @@
 #define RA02_RegPaConfig 0x09
 #define RA02_RegPaRamp 0x0A
 #define RA02_RegOcp 0x0B
-
-
-
+#define RA02_RegLna 0x0C
+#define RA02_RegFifoAddrPtr 0x0D
+#define RA02_RegFifoTxBaseAddr 0x0E
+#define RA02_RegFifoRxBaseAddr 0x0F
+#define RA02_RegFifoRxCurrentAddr 0x10
+#define RA02_RegIrqFlagsMask 0x11
+#define RA02_RegIrqFlags 0x12
+#define RA02_RegRxNbBytes 0x13
+#define RA02_RegRxHeaderCntValueMsb 0x14
+#define RA02_RegRxHeaderCntValueLsb 0x15
+#define RA02_RegRxPacketCntValueMsb 0x16
+#define RA02_RegRxPacketCntValueLsb 0x17
+#define RA02_RegModemStat 0x18
+#define RA02_RegPktSnrValue 0x19
+#define RA02_RegPktRssiValue 0x1A
+#define RA02_RegRssiValue 0x1B
+#define RA02_RegHopChannel 0x1C
+#define RA02_RegModemConfig1 0x1D
+#define RA02_RegModemConfig2 0x1E
+#define RA02_RegSymbTimeoutLsb 0x1F
+#define RA02_RegPreambleMsb 0x20
+#define RA02_RegPreambleLsb 0x21
+#define RA02_RegPayloadLength 0x22
+#define RA02_RegMaxPayloadLength 0x23
+#define RA02_RegHopPeriod 0x24
+#define RA02_RegFifoRxByteAddrPtr 0x25
+#define RA02_RegModemConfig3 0x26
+#define RA02_RegPpmCorrection 0x27
+#define RA02_RegFeiMsb 0x28
+#define RA02_RegFeiMid 0x29
+#define RA02_RegFeiLsb 0x2A
+#define RA02_RegRssiWideband 0x2C
+#define RA02_RegDetectOptimize 0x31
+#define RA02_RegInvertIQ 0x33
+#define RA02_RegDetectionThreshold 0x37
+#define RA02_RegSyncWord 0x39
 
 
 
